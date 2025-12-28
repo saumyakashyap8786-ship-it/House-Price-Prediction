e2bcaa54eb8b83cfe6cd9f78d0438b87568ed13c
saumyakashyap8786-ship-it <saumyakashyap8786@gmail.com>
2025-12-28 12:32:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,8 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
 st.sidebar.title('Select House features: ')
+st. sidebar.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+all_value = []
+for i in final_X:
+result = st.sidebar.slider(f'Selec6{i} value')
+all_value.append(result)
